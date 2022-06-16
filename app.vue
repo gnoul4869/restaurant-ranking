e2d2b684d3202125e9dcd3b5422826ad4ccc022e
gnoul_ <ttluong4869@gmail.com>
2022-06-17 00:39:15 +0700

    Initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+    <Nav />
+    <div class="container text-center mt-5">
+        <h1>Restaurant Ranking</h1>
+    </div>
+</template>
---
+++ components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Restaurant Ranking</a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/restaurants">Restaurants</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
